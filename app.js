0d9c734094f174a745555d8470b25c888a164017
luthfiajiw <luthfi.ajiw@gmail.com>
2018-11-25 05:56:39 +0700

    operasi insert,update,delete data

--- app.js
+++ app.js
@@ -26,6 +26,54 @@ routes.addRoute('/', function(req, res) {
   });
 });
 
+routes.addRoute('/insert', function(req, res) {
+  connection.query("insert into mahasiswa set ?", {
+    no_induk: "123456799",
+    nama: "Afin Dwi",
+    alamat: "Beran"
+  }, function(err, field) {
+    if(err) throw err;
+
+    let html = view.compileFile('./template/index.html')({
+      h1: field.affectedRows + " Rows Affected"
+    });
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.end(html);
+  })
+
+
+});
+
+routes.addRoute('/update', function(req, res) {
+  connection.query("update mahasiswa set ? where ?", [
+    {nama: "Afin Dwi Widandari"},
+    {no_induk: "123456799"}
+  ], function(err, fields) {
+    if (err) throw err;
+
+     let html = view.compileFile('./template/index.html')({
+       h1: fields.affectedRows + " Rows Updated"
+     });
+     res.writeHead(200, {"Content-Type" : "text/html"});
+     res.end(html);
+
+  })
+});
+
+routes.addRoute('/delete', function(req, res) {
+  connection.query("delete from mahasiswa where ?",
+  {no_induk: "123456789"},
+  function(err, fields) {
+    if (err) throw err;
+
+    let html = view.compileFile('./template/index.html')({
+      h1: fields.affectedRows + " Rows Deleted"
+    });
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.end(html);
+  });
+});
+
 http.createServer(function (req,res) {
   let pathname = url.parse(req.url).pathname;
   let match = routes.match(pathname);
